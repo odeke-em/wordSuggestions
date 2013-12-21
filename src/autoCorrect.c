acc06b303327a5c1614e5471fd384191d25d0186
Emmanuel Odeke <odeke@ualberta.ca>
2013-12-20 18:51:49 -0700

    Proper free order

--- src/autoCorrect.c
+++ src/autoCorrect.c
@@ -20,22 +20,28 @@ int main() {
 
   Trie *memoizeTrie = createTrie(0);
 
-  FILE *ifp = fopen(__FILE__, "r");
+  FILE *ifp = fopen("src/test.c", "r");
   while (! feof(ifp)) {
     char *inW = getWord(ifp);
-    if (inW == NULL) continue;
 
-    if (searchTrie(memoizeTrie, inW) != -1) continue; // Word already discovered
+    if (inW != NULL) {
+      if  (searchTrie(memoizeTrie, inW) == -1) {
+	// Word not yet discovered
+	Element *match = getCloseMatches(inW, dict, 0.8);
+	printf("%s {\n", inW);
+	while (match != NULL) {
+	  printf("\t%s\n", (char *)match->value);
+	  match = getNext(match);
+	} 
 
-    Element *match = getCloseMatches(inW, dict, 0.8);
-    printf("%s {\n", inW);
-    while (match != NULL) {
-      printf("\t%s\n", (char *)match->value);
-      match = getNext(match);
-    } 
-    printf("}\n");
-    memoizeTrie = addSequence(memoizeTrie, inW);
-    if (inW != NULL) free(inW);
+	printf("}\n");
+
+	// Mark the word now as discovered
+	memoizeTrie = addSequence(memoizeTrie, inW);
+      }
+
+      free(inW);
+    }
   }
 
 #ifdef DEBUG
@@ -44,8 +50,8 @@ int main() {
 
   // CleanUp
   destroyHashList(dict);
-  destroyTrie(memoizeTrie);
+  memoizeTrie = destroyTrie(memoizeTrie);
 
-  fclose(ifp);
+  if (ifp != NULL) fclose(ifp);
   return 0;
 }
