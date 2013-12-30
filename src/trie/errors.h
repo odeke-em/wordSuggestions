374a30b095bf7733c47b741a5e170321ff2d76ff
Emmanuel Odeke <odeke@ualberta.ca>
2013-12-30 00:37:19 -0700

    Updated */errors.h files

--- src/trie/errors.h
+++ src/trie/errors.h
@@ -2,11 +2,11 @@
 #define _ERRORS_H
   #include <stdio.h>
 
-  typedef enum{
+  typedef enum {
     TypeError, ValueError, IndexError, SyntaxError, BufferOverFlow,
     AssertionError, NullPointerException, IndexOutOfBoundsException,
     ZeroDivisionException, CorruptedDataException
-  }Exception;
+  } Exception;
   
   #define raiseWarning(errMsg){\
     fprintf(stderr,"\033[31m%s on line %d in function '%s' file '%s'\033[00m\n",\
@@ -15,22 +15,26 @@
 
   #define throwException(errCode,errMsg){\
     if (errMsg != NULL){\
-      fprintf(stderr, "%s\n", errMsg);\
+      fprintf(stderr, "%s\n", #errMsg);\
     }\
     raiseWarning(#errCode);\
     exit(-1);\
   }
  
   #define assert(validExpression){\
-    if (! validExpression)\
-      raiseError(validExpression);\
+    if (! (validExpression))\
+      raiseError((validExpression));\
   }
 
   #define raiseError(args) {\
     fprintf(stderr, "Traceback most recent call at line: %d ", __LINE__);\
-    fprintf(stderr, "of file: %s\nExpression: %s is invalid\n", \
-	    __FILE__, #args);\
+    fprintf(stderr, "of file: %s\n\033[31m%s\033[00m\n", \
+         __FILE__, #args);\
     exit(-2);\
-  }\
-   
+  }
+
+  #define raiseExceptionIfNull(expression){\
+    if (! expression)\
+      throwException(NullPointerException, expression);\
+  }
 #endif
