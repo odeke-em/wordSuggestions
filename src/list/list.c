be485b220b9d9990802f088e9a358084e1df5ab8
Emmanuel Odeke <odeke@ualberta.ca>
2014-01-12 03:07:25 -0700

    Multi-merge

--- src/list/list.c
+++ src/list/list.c
@@ -1,19 +1,39 @@
 // Author: Emmanuel Odeke <odeke@ualberta.ca>
-#include <stdlib.h>
 #include <stdio.h>
-#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "errors.h"
 
 #include "list.h"
+#define DEMO_CIRCULARITY
 
 inline void *getData(const Node *n) { 
   return n == NULL ? NULL : n->data; 
+}
+
+inline Tag getTag(const Node *n) {
+  return n == NULL ? Unknown : n->tag;
+}
+
+inline void *peek(const List *l) {
+  return isEmpty(l) ? NULL : getData(l->head);
+}
+
+inline int isEmpty(const List *l) {
+  return (l== NULL || l->head == NULL || l->size == 0) ? 1 : 0;
 }
 
 inline void *getNextNode(const Node *n) { 
   return n == NULL ? NULL : n->next; 
 }
 
-inline int getListSize(const List *l) { 
+void swap(void **a, void **b) {
+  void *tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+inline unsigned int getListSize(const List *l) { 
   return l == NULL ? 0 : l->size; 
 }
 
@@ -21,16 +41,28 @@ inline Node *allocNode(void) {
   return (Node *)malloc(sizeof(Node)); 
 }
 
+void *listPop(Node **n) {
+  void *listPopd = NULL;
+  if (n != NULL && *n != NULL) {
+    listPopd = (*n)->data;
+    Node *nextTmp = (*n)->next; 
+    free(*n);
+    *n = nextTmp;
+  }
+
+  return listPopd;
+}
+
 Comparison intPtrComp(const void *i1, const void *i2) {
-  if (i1 == NULL && i2 == NULL) return EQUAL;
+  if (i1 == NULL && i2 == NULL) return Equal;
   else if (i1 != NULL && i2 != NULL) {
     const int *localI1 = (int *)i1;
     const int *localI2 = (int *)i2;
-    if (*localI1 != *localI2) return *localI1 < *localI2 ? LESSTHAN : GREATERTHAN;
-    else return EQUAL;
+    if (*localI1 != *localI2) return *localI1 < *localI2 ? Less : Greater;
+    else return Equal;
   }
 
-  return i1 == NULL ? LESSTHAN : GREATERTHAN;
+  return i1 == NULL ? Less : Greater;
 }
 
 Node *initNode(Node *n) {
@@ -45,7 +77,7 @@ Node *initNode(Node *n) {
 
 Node *createNewNode(void) {
   Node *newNode = allocNode();
-  assert(newNode != NULL);
+  assert(newNode);
   
   newNode = initNode(newNode);
   return newNode;
@@ -54,8 +86,8 @@ Node *createNewNode(void) {
 List *initList(List *l) {
   if (l != NULL) {
     l->size = 0;
-    l->head = initNode(l->head);
-    l->tail = initNode(l->tail);
+    l->head = NULL;
+    l->tail = NULL;
   }
 
   return l;
@@ -63,7 +95,7 @@ List *initList(List *l) {
 
 List *createNewList(void) {
   List *newList = allocList();
-  assert(newList != NULL);
+  assert(newList);
   
   newList = initList(newList);
   return newList;
@@ -74,8 +106,47 @@ inline List *allocList(void) {
   return (List *)malloc(sizeof(List));
 }
 
+List *append(List *l, void *data) {
+  return appendAndTag(l, data, Heapd, NULL);
+}
+
+List *appendWithFreer(List *l, void *data, void (*freer)(void *)) {
+  return appendAndTag(l, data, Heapd, freer);
+}
+
+List *appendAndTag(List *l, void *data, Tag tag, void (*freer)(void *)) {
+  if (l == NULL) {
+    l = createNewList();
+  }
+
+  if (l->head == NULL) {
+    // First item being added to the list
+    l->head = createNewNode();
+    l->head->data = data;
+    l->head->tag = tag;
+    l->tail = NULL;
+    l->head->next = l->tail;
+  } else if (l->tail == NULL) {
+    l->tail = createNewNode(); 
+    l->tail->data = data;
+    l->tail->tag = tag;
+    l->head->next = l->tail;
+  } else {
+    l->tail->next = createNewNode();
+    l->tail = l->tail->next;
+    l->tail->tag = tag;
+    l->tail->data = data;
+  }
+
+  ++l->size;
+  return l;
+}
 
 List *prepend(List *l, void *data) {
+  return prependAndTag(l, data, Heapd);
+}
+
+List *prependAndTag(List *l, void *data, Tag tag) {
 #ifdef DEBUG
   printf("\033[32m%s\033[00m\n", __func__);
 #endif
@@ -88,12 +159,14 @@ List *prepend(List *l, void *data) {
     // First item being added to the list
     l->head = createNewNode();
     l->head->data = data;
+    l->head->tag = tag;
     l->tail = l->head;
   } else {
     // Adding to the front
     Node *newEnd = createNewNode();
     newEnd->data = data;
     newEnd->next = l->head;
+    newEnd->tag = tag;
     l->head = newEnd;
     l->tail->next = l->head;
   }
@@ -111,72 +184,186 @@ int freeFromHeadToTail(Node *head, Node *tail) {
 
 void destroyList(List *l) {
   if (l != NULL) {
-    Node *start = l->head, *end = l->tail, *tmp;
+    Node *start = l->head, *end = l->tail, *tmp = NULL;
 
-    while (start != end) {
-      tmp = start->next;
-      if (start == NULL) break;
+    if (start != NULL) {
+      void (*dataFreer)(void *) = NULL;
+      while (start != end) {
+	tmp = start->next;
+	if (start == NULL) break;
 
-      if (start->data != NULL)  {
-	if (start->freeData == NULL) 
-	  free(start->data);
-	else
-	  start->freeData(start->data);
+	if (start->tag == Heapd && start->data != NULL)  {
+	  if (start->freeData == NULL) 
+	    free(start->data);
+	  else
+	    start->freeData(start->data);
+	}
 
+	free(start);
+	start = tmp;
       }
-      free(start);
-      start = tmp;
+
+      if (end != NULL) {
+	if (end->data != NULL) free(end->data);
+	free(end);
+      }
     }
 
-    if (end != NULL) {
-      if (end->data != NULL) free(end->data);
-      free(end);
-    }
+    l->tail = NULL; // No more access to the tail either
 
     free(l);
     l = NULL;
   }
 }
 
-void printList(List *l) {;
+void printList(List *l) {
   printf("[");
   if (l != NULL) {
     Node *it = l->head, *end = l->tail;
-    do {
-      if (it == NULL) break;
+    while (it != NULL) {
       if (it->data  != NULL) printf(" %d:%d ", *(int *)it->data, it->tag);
+      if (it == end) break;
       it = it->next;
-    } while (it != end);
+    }
   }
   printf("]");
 }
 
-Node *find(List *l, void *query, Comparator matchFunc) {;
+List *multiMerge(unsigned int count, ...) {
+  // Pass the number of lists to merge, the lists themselves
+  // and the very last argument should be the Comparator
+  List *merged = NULL;
+#ifdef DEBUG
+  printf("Func:: %s\n", __func__);
+#endif
+
+  va_list ap;
+  va_start(ap, count);
+
+  List **lBlock = (List **)malloc(sizeof(List *) * count);
+  List *tmpL = NULL;
+
+  int i=0;
+  while (i < count) {
+    tmpL = va_arg(ap, List *);
+    lBlock[i++] = tmpL;
+  }
+
+  Comparator matchFunc = va_arg(ap, Comparator);
+
+#ifdef DEBUG
+  printf("ap: %p\n", ap);
+#endif
+
+  va_end(ap);
+  if (i) {
+    int popularCount = count, midIndex = i/2, maxCount = i, gallop;
+    int thresholdCount = 0.75 * count;
+    List **minList = NULL;
+    while (popularCount >= thresholdCount) {
+    #ifdef DEBUG
+      for (i=0; i < maxCount; ++i) {
+	printList(lBlock[i]); 
+	printf("\n");
+      }
+      printf("\033[35m\nBlockEntries\n\033[00m");
+    #endif
+
+      popularCount = maxCount;
+      pickMinList: {
+	int minIdx=0;
+	while (minIdx < maxCount) {
+	  minList = lBlock + minIdx;
+	  if (peek(*minList) != NULL) {
+	    break;
+	  } else {
+	    ++minIdx;
+	  }
+	}
+
+	if (peek(*minList) == NULL) {
+	  printf("No more selections for minList can be made\n");
+	  goto cleanUpForReturn;
+	}
+      }
+
+      int gallop = 0, nonEmptyCount = 0;
+      for (i=0; i < maxCount; ++i) {
+	if (peek(lBlock[i]) == NULL) {
+	  --popularCount;
+	} else {
+	  ++nonEmptyCount;
+	  Comparison comp = matchFunc(peek(*minList), peek(lBlock[i]));
+	  if (comp == Equal) {
+	    ++gallop;
+	  } else if (comp == Greater) {
+	    minList = lBlock + i;
+	  }
+	}
+      }
+
+      if (gallop == nonEmptyCount) { // All elements were equal
+	for (i=0; i < maxCount; ++i) {
+	  if (peek(lBlock[i]) != NULL) {
+	    merged = append(merged, popHead(lBlock[i]));
+	  }
+	}
+      } else {
+	merged = append(merged, popHead(*minList));
+      }
+    }
+
+    for (i=0; i < maxCount; ++i) {
+      while (peek(lBlock[i]) != NULL) {
+	merged = append(merged, popHead(lBlock[i]));
+      }
+    }
+  }
+
+  cleanUpForReturn: {
+    free(lBlock);
+  }
+  
+  return merged;
+}
+
+Node *find(List *l, void *query, Comparator matchFunc) {
   Node *result = NULL;
   if (l != NULL && matchFunc != NULL) {
     Node *it = l->head, *end = l->tail;
     do {
       if (it == NULL) break;
     
-      if (matchFunc(it->data, query) == EQUAL) {
-	result = it;
-	break;
+      if (matchFunc(it->data, query) == Equal) {
+        result = it;
+        break;
       }
       it = it->next;
-    } while (it != NULL);
+    } while (it != end);
   }
 
   return result;
 }
 
+void *popHead(List *l) {
+  void *popd = NULL;
+  if (l != NULL && l->head != NULL) {
+    popd = listPop(&l->head);
+    --l->size;
+  }
+
+  return popd;
+}
+
 List *removeElem(List *l, void *query, Comparator matchFunc) {
   if (l != NULL && matchFunc != NULL) {
-    Node *prev = NULL, *cur = l->head;
+    Node *prev = NULL, *cur = l->head, *end = l->tail;
     while (cur != NULL) {
-      if (matchFunc(cur->data, query) == EQUAL) break;
+      if (matchFunc(cur->data, query) == Equal) break;
 
       prev = cur;
       cur = cur->next;
+      if (cur == end) break;
     }
 
     if (cur != NULL) {
@@ -189,35 +376,49 @@ List *removeElem(List *l, void *query, Comparator matchFunc) {
       if (cur->data != NULL) free(cur->data);
       free(cur);
       cur = NULL;
+      --l->size;
     }
-    
   }
   return l;
 }
+
+
 #ifdef SAMPLE_RUN
 int main() {
   int i;
   List *l = NULL;
+
+  List *(* listAddFunc)(List *, void *) = append;
+#ifdef DEMO_PREPEND
+  listAddFunc = prepend;
+#endif
+
   for (i=0; i < 40; ++i) {
     int *tp = (int *)malloc(sizeof(int));
     *tp = i;
-    l = prepend(l, tp);
-  #ifdef DEBUG
-    printf("Aprespend: %p tp: %p\n", l, tp);
-  #endif
+    l = listAddFunc(l, tp);
   }
 
-  Node *start = l->head, *end = l->tail;
-  while (start != end) {
-    start = start->next;
+#ifdef DEMO_CIRCULARITY
+  while (l->head != NULL) {
+    void *vSav = listPop(&(l->head));
+    if (vSav != NULL) {
+      printf("vS: %d\n", *(int *)vSav);
+      free(vSav);
+    }
+    break;
   }
+#endif
 
   printList(l);
   printf("\n");
 
   i = 8;
+
+  printf("Size: %d\n", getListSize(l));
   l = removeElem(l, &i, intPtrComp);
 
+  printf("Size: %d\n", getListSize(l));
   printList(l);
   printf("\n");
   destroyList(l);
--- src/list/list.h
+++ src/list/list.h
@@ -1,19 +1,24 @@
 // Author: Emmanuel Odeke <odeke@ualberta.ca>
 #ifndef _LIST_H
 #define _LIST_H
+  #include <stdarg.h>
   typedef enum {
-    LESSTHAN=-1, EQUAL=0, GREATERTHAN=1
+    Less=-1, Equal=0, Greater=1
   } Comparison;
 
+  typedef enum Bit_ {
+    Unknown=-1, Stackd=0, Heapd=1
+  } Tag;
+
   typedef struct Node_ {
+    Tag tag;
     void *data;
     struct Node_ *next;
-    unsigned int tag:1;
     int (*freeData)(void *data);
   } Node;
 
   typedef struct List_ {
-    int size;
+    unsigned int size;
     Node *head, *tail;
   } List;
 
@@ -29,15 +34,39 @@
 
   inline void *getData(const Node *n);
   inline void *getNextNode(const Node *n);
-  inline int getListSize(const List *l);
+  inline Tag getTag(const Node *n);
+  inline unsigned int getListSize(const List *l);
 
+  void *listPop(Node **n);
+  void *popHead(List *l);
+  inline void *peek(const List *l);
+
+  List *appendAndTag(List *l, void *data, Tag tag, void (*freer)(void *));
+  List *appendWithFreer(List *l, void *data, void (*freer)(void *));
+
+  // If no tag is explicitly provided, it is assumed that data is memory
+  // obtained by a call to malloc or an object that exists on the heap
+  List *append(List *l, void *data);
+
+  List *prependAndTag(List *l, void *data, Tag tag);
+
+   // If no tag is explicitly provided, it is assumed that data is memory
+  // obtained by a call to malloc or an object that exists on the heap
   List *prepend(List *l, void *data);
+
   Node *find(List *l, void *query, Comparator comp);
   List *removeElem(List *l, void *query, Comparator comp);
 
+  inline int isEmpty(const List *l);
   void destroyList(List *l);
 
   // Miscellaneous
   void printList(List *l);
   Comparison intPtrComp(const void *i1, const void *i2);
+
+  // Pass the number of lists to merge, the lists themselves and
+  // the very last argument should be the Comparator
+  List *multiMerge(unsigned int count, ...);
+
+  void swap(void **a, void **b);
 #endif
