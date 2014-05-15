c3f4f2919161e378a02efb08c30bd1f46b34df2b
Emmanuel Odeke <odeke@ualberta.ca>
2014-05-15 16:20:39 -0600

    Parallelizing getMatches

--- src/hashlist/loadWords.c
+++ src/hashlist/loadWords.c
@@ -1,8 +1,9 @@
 // Author: Emmanuel Odeke <odeke@ualberta.ca>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <pthread.h>
 
 #include "hashList.h"
 #include "loadWords.h"
@@ -106,9 +107,84 @@ HashList *loadWordsInFile(const char *filePath) {
   return hl;
 }
 
+void *subUnitMatch(void *data) {
+  Element *resultL = NULL;
+  MatchUnit *mu = (MatchUnit *)data;
+  if (mu != NULL) {
+    Element **trav = mu->start;
+    int threshold = mu->ownRank * mu->threshold;
+    while (trav < mu->end && trav < mu->absEnd) {
+      if (*trav != NULL && (*trav)->value != NULL) {
+        int rank = getRank(mu->query, (*trav)->value);
+        if (rank >= threshold) {
+           resultL = addToHeadWithRank(resultL, (*trav)->value, (double)rank/mu->ownRank);
+        }
+      } 
+
+      ++trav;
+    }
+  }
+
+  return (void *)resultL;
+}
+
+Element *mergeEL(Element *from, Element *to) {
+  if (from != NULL) {
+    Element *saveNext = NULL;
+    while (from != NULL) {
+      to = addToHeadWithRank(to, from->value, from->rank);
+      saveNext = from->next;
+      free(from); // The data in from is inherited by 'to'
+      from = saveNext;
+    }
+  }
+
+  return to;
+}
+
+Element *matchesOnUniThreadOnMultiThread(
+  const char *query, HashList *dict, const unsigned int ownRank,
+  const double percentMatch, const unsigned int thCount
+) {
+  Element *mergedL = NULL;
+  if (query != NULL && dict != NULL && dict->size >= 1 && thCount >= 1) {
+    pthread_t thL[thCount];
+    MatchUnit muL[thCount];
+
+    unsigned int i=0, stepSize=dict->size/thCount, stepCounter=0;
+
+    while (stepCounter < dict->size && i < thCount) {
+      MatchUnit *mu = &muL[i];
+      mu->absEnd = dict->list + dict->size;
+      mu->start =  dict->list + stepCounter;
+      mu->ownRank = ownRank;
+      mu->threshold = percentMatch; 
+
+      stepCounter += stepSize;
+      mu->end    =  dict->list + stepCounter;
+
+      pthread_create(&thL[i++], NULL, subUnitMatch, mu);
+    }
+
+    unsigned int j;
+    int joinStatus;
+    for (j=0; j < i; ++j) {
+      Element *dataRecv=NULL;
+      joinStatus = pthread_join(thL[j], (void *)&dataRecv);
+      if (joinStatus) {
+        fprintf(stderr, "\033[91mFailed to join thread: %d. Status:: %d\n", j, joinStatus);
+      }
+      else {
+        mergedL = mergeEL(dataRecv, mergedL);
+      }
+    }
+  }
+
+  return mergedL;
+}
 // Do not free data returned from this function as it just contains pointers
 // to data associated with the dictionary that will be explicitly freed
-Element *matches(
+Element *matchesOnUniThread(
   const char *query, HashList *dict, 
   const unsigned int ownRank, const double percentMatch
 ) {
@@ -122,8 +198,8 @@ Element *matches(
     if (*matchList != NULL) { // An entry with the same hash value found
       // In case of any collisions, strcmp should help sort things out
       if ((*matchList)->value && strcmp(query, (*matchList)->value) == 0) {
-	// Absolute match found
-	matchFound = 1;
+        // Absolute match found
+        matchFound = 1;
       }
     }
 
@@ -132,14 +208,14 @@ Element *matches(
       double threshHold = ownRank * percentMatch;
       Element **trav = dict->list, **end = trav + getSize(dict);
       while (trav != end) {
-	if (*trav != NULL && (*trav)->value != NULL) {
-	  int rank = getRank(query, (*trav)->value);
-	  if (rank >= threshHold) {
-	    matchL = \
+        if (*trav != NULL && (*trav)->value != NULL) {
+          int rank = getRank(query, (*trav)->value);
+          if (rank >= threshHold) {
+            matchL = \
               addToHeadWithRank(matchL, (*trav)->value, (double)rank/ownRank);
-	  } 
-	}
-	++trav;
+          } 
+        }
+        ++trav;
       }
     } else {
       matchL = *matchList;
@@ -157,7 +233,13 @@ Element *getCloseMatches(
     return NULL;
   } else {
     int ownRank = getRank(query, query);
-    return matches(query, dict, ownRank, percentMatch);
+
+  #ifdef RUN_ON_SINGLE_THREAD
+    return matchesOnUniThread(query, dict, ownRank, percentMatch);
+  #else
+    // TODO: Pass in the thread count
+    return matchesOnUniThreadOnMultiThread(query, dict, ownRank, percentMatch, 4);
+  #endif
   }
 }
 
@@ -175,8 +257,8 @@ int main() {
       Element *match = getCloseMatches(value, hl, 0.6);
       printf("For: %s %p\n", value, curFileHL->list[i]);
       while (match != NULL) {
-	printf("\t%s\n", (char *)match->value);
-	match = getNext(match);
+         printf("\t%s\n", (char *)match->value);
+         match = getNext(match);
       }
     }
   }
--- src/hashlist/loadWords.h
+++ src/hashlist/loadWords.h
@@ -4,9 +4,24 @@
   #include "hashList.h"
   #include "wordTransition.h"
 
+    typedef struct {
+        char *query;
+        unsigned int ownRank;
+        double threshold;
+        Element **start, **end, **absEnd; 
+    } MatchUnit;
+
+    void *subUnitMatch(void *data);
+    Element *mergeEL(Element *from, Element *to);
+
+    Element *matchesOnUniThreadOnMultiThread(
+      const char *query, HashList *dict, const unsigned int ownRank,
+      const double percentMatch, const unsigned int thCount
+    );
+
   char *getWord(FILE *ifp, int *lenStorage);
   HashList *loadWordsInFile(const char *filePath);
-  Element *matches(
+  Element *matchesOnUniThread(
     const char *query, HashList *dict, 
     const unsigned int ownRank, const double threshHold
   );
