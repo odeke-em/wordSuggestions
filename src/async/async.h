d2909c678d04aa0a2c60f65aa159445d231b4d1b
Emmanuel Odeke <odeke@ualberta.ca>
2014-06-03 18:34:06 -0600

    Cleaning out unused code

--- src/async/async.h
+++
@@ -1,9 +0,0 @@
-// Author: Emmanuel Odeke <odeke@ualberta.ca>
-#ifndef _ASYNC_H
-#define _ASYNC_H
-  typedef struct {
-    void *args;
-    void *result;
-    void * (*func)(void *);
-  } thArg;
-#endif
